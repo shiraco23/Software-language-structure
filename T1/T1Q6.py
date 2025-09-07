b5ee5cd96f8fe375e4bd1c085e598dcddaf4c1a9
shiraco23 <154763000+shiraco23@users.noreply.github.com>
2025-09-07 22:46:39 +0300

    Restore comments and example test in T1Q6.py

--- T1/T1Q6.py
+++ T1/T1Q6.py
@@ -6,4 +6,6 @@ unar_func = [("X*2",lambda x: x*2), ("X**2", lambda x: x**2), ("X**(-1)", lambda
 def unar_dict(lst, unar_func):
     return dict(map(lambda f: (f[0], list(map(f[1], lst))), unar_func))
 
-print(unar_dict([1,2,3,4,5], unar_func))
+
+# Example test:
+# print(unar_dict([1,2,3,4,5], unar_func))
